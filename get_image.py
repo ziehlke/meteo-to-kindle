faab21036229c6e96e0af8145d4d9955b8701479
Paweł Ziehlke <pawel.ziehlke@ringieraxelspringer.pl>
2020-04-25 15:33:09 +0200

    update meteo address

--- get_image.py
+++ get_image.py
@@ -1,75 +1,65 @@
-#!/usr/bin/python
-import urllib
-from time import sleep
 import subprocess
-import shlex
+import urllib.request
+from time import sleep
+from urllib.error import ContentTooShortError
+
 from PIL import Image
 
 
-def download(url):
-    urllib.urlretrieve(
-        url, "/mnt/OpenShare/weather/weather-script-output.png")
-    return None
-
-
-def crop(img):
-    img_down = img.crop((0, 524, 585, 635))
+def crop(image):
+    img_down = image.crop((0, 524, 585, 635))
     img_down.load()
-    img.paste(img_down, (0, 400, 585, 511))
-    img = img.crop((35, 0, 540, 511))
-    img.load()
-    return img
+    image.paste(img_down, (0, 400, 585, 511))
+    image = image.crop((35, 0, 540, 511))
+    image.load()
+    return image
 
 
-def removeLogo(img):
-    pixdata = img.load()
-    for y in range(img.size[1]):
-        for x in range(img.size[0]):
+def removeLogo(image):
+    pixdata = image.load()
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
             if pixdata[x, y] == (255, 251, 240) or pixdata[x, y] == (244, 244, 244):
                 pixdata[x, y] = (255, 255, 255)
             elif pixdata[x, y] == (216, 216, 216) or pixdata[x, y] == (215, 216, 215):
                 pixdata[x, y] = (226, 226, 226)
-    return img
+    return image
 
 
-def adjustSize(img):
-    toWidth = 600
-    ratio = (toWidth / float(img.size[0]))
-    newHeight = int((float(img.size[1]) * float(ratio)))
-    img = img.resize((toWidth, newHeight), Image.ANTIALIAS)
+def adjustSize(image):
+    to_width = 600
+    ratio = (to_width / float(image.size[0]))
+    new_height = int((float(image.size[1]) * float(ratio)))
+    image = image.resize((to_width, new_height), Image.ANTIALIAS)
     template = Image.open("/mnt/OpenShare/weather/template.png")
-    template.paste(img)
+    template.paste(image)
     return template
 
 
 if __name__ == '__main__':
-    # 346, 210 - for Gdansk
-    url = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row={}&col={}&lang=pl".format(
-        346, 210)
-    download(url)
+    #  Gdansk = [346, 210]
+    Krakow = [466, 232]
+    url = f"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&" \
+          f"row={Krakow[0]}&" \
+          f"col={Krakow[1]}&" \
+          f"lang=pl"
+    output = "/mnt/OpenShare/weather/weather-script-output.png"
+
     while True:
         try:
-            img = Image.open("/mnt/OpenShare/weather/weather-script-output.png")
+            urllib.request.urlretrieve(url, output)
+            img = Image.open(output)
             img = img.convert("RGB")
-        except (SyntaxError, OSError):
+        except [SyntaxError, OSError, ContentTooShortError]:
             print("\nDownload failed... retry in 15 seconds.\n")
             sleep(15)
-            download(url)
-            img = Image.open("/mnt/OpenShare/weather/weather-script-output.png")
-            img = img.convert("RGB")
             continue
         break
 
     img = crop(img)
     img = removeLogo(img)
     img = adjustSize(img)
-    img.save("/mnt/OpenShare/weather/weather-script-output.png", bits=8)
+    img.save(output, bits=8)
 
-    
-    bash = shlex.split('/usr/bin/pngcrush -c 0 -ow "/mnt/OpenShare/weather/weather-script-output.png"')
-    subprocess.call(bash, shell=True)
-
-
-
-    # bash = ('/usr/bin/pngcrush', '-c', '0', '-ow', "/mnt/OpenShare/weather/weather-script-output.png")
-    # subprocess.Popen(bash, stdout=subprocess.PIPE)
+    subprocess.run(['pngcrush', '-c', '0', output])
+    subprocess.run(['mv', 'pngout.png', output])
